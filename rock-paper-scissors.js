a8edadc2bd031ffee09c9e2c0f0a12e0e95ff675
devwellington <devwellington@gmail.com>
2014-05-25 16:39:30 -0300

    Create rock-paper-scissors.js
    Initial commit.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,37 @@
+var userChoice = prompt("Voce escolhe pedra, papel ou tesoura?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "pedra";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "papel";
+} else {
+	computerChoice = "tesoura";
+} console.log("Computer: " + computerChoice);
+
+
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+        return "O resultado e um empate";
+    } else if (choice1 === "pedra"){
+        if (choice2 === "tesoura"){
+            return "pedra vence";
+        } else {
+            "papel vence";
+        }
+    } else if (choice1 === "papel"){
+        if (choice2 === "pedra"){
+            return "papel vence";    
+        } else{
+            return "tesoura vence";
+        }
+        
+    } else if (choice1 === "tesoura"){
+        if (choice2 === "pedra"){
+            return "pedra vence";    
+        } else if (choice2 === "papel"){
+            return "tesoura vence";    
+        }        
+    }
+};
+
+compare(userChoice, computerChoice);
